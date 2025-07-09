efa9c8159a0fcd50b79f95ab2f9f59313aa7609f
veerababu <jlveerababu99@gmail.com>
2025-07-09 23:00:25 +0530

    variables

--- 02-converstation.sh
+++ 02-converstation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Ramesg:: Hi Suresh, How are you?"
+echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
 echo "Ramesh:: i am doing good suresh. What's going on?"
 echo "Suresh:: I started learning shell Script Ramesh"
