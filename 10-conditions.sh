5b02f33999c54b4fd44415e9fd3d9e7439decd37
veerababu <jlveerababu99@gmail.com>
2025-08-07 17:14:02 +0530

    functions

---
+++ 10-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt ] #gt, lt, eq, -ne, -ge, -le
+then
+
+    echo "Given number: $NUMBER is grater then 20"
+
+    else 
+     
+     echo "Given number: $NUMBER is less then  20"
+
+     fi
