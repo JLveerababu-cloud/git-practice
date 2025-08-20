7fa0a04969e33a239d87d1fb434f0c8272829b2a
veerababu <jlveerababu99@gmail.com>
2025-08-20 23:04:31 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -29,7 +29,21 @@ VALIDATE(){
     fi
 }
 
+USAGE(){
+
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
+    exit 1
+
+}
+
 CHECK_ROOT
+
+if [ $# -eq 0 ]
+ then
+
+        USAGE
+
+ fi
 # sh 15-loops.sh git mysql postfix nginx
 for package in $@ # $@ refers to all arguments passed to it
 do
