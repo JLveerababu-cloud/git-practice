56eafee2527aebee5a7219e9c1e4608761deb4a7
veerababu <jlveerababu99@gmail.com>
2025-10-03 15:55:08 +0530

    shell

---
+++ 19-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #if $3 is empty, default is 14 days
+
+USAGE(){
+    echo -e "USAGE : : sh 19-backup.sh <source> <destination> <days(optional)>"
+}
+
+# check the source and destnation are provided
+
+if[  $# -lt 2 ]
+then
+  echo  
+
+  USAGE
+
